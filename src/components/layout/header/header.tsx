bb02efdc9eb7a2e7d476a4decd2842122acd9de2
Esther Rose <estherrose046@gmail.com>
2023-05-16 13:34:11 -0500

    updated header

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,25 +1,36 @@
-import {useContext} from 'react';
-import { Button, ButtonVariant } from "../../UI/button/Button"
-import { BoardContext } from "../../UI/board/create-new-board/board-context";
-const headerStyles = 'box-border flex flex-1 dark:bg-darkGrey items-center px-5 gap-5 basis-full justify-between font-semibold w-screen h-[100px] bg-white border-b border-lightLines dark:border-darkLines'
-const currentBoardLabel = 'flex flex-1 text-black dark:text-white tracking-normal  text-2xl'
+import { useContext } from 'react';
+import { Button, ButtonVariant } from '../../UI/button/Button';
+import { BoardContext } from '../../UI/board/create-new-board/board-context';
+const headerStyles =
+  'box-border flex flex-1 dark:bg-darkGrey items-center px-5 gap-5 basis-full justify-between font-semibold w-screen h-[100px] bg-white border-b border-lightLines dark:border-darkLines';
+const currentBoardLabel = 'flex flex-1 text-black dark:text-white tracking-normal  text-2xl';
 const logoStyle = 'box-border flex-1 m-0 w-[113px] min-w-max p-4 dark:bg-darkGrey';
-const logoWrapper = 'box-border border-lightLines dark:border-darkLines  border-r w-[300px]'; 
+const logoWrapper = 'box-border border-lightLines dark:border-darkLines  border-r w-[300px]';
 
 type HeaderProps = {
-    isDarkMode: boolean; 
-}
+  isDarkMode: boolean;
+};
 export default function Header(props: HeaderProps) {
-    const {isDarkMode} = props; 
-    const {selectedBoard} = useContext(BoardContext);
-    return (
-        <div className={headerStyles}>
-    {isDarkMode ? <div className={logoWrapper}><img className={logoStyle} src="/icons/logo-light.svg"/> </div>: <div className={logoWrapper}><img className={logoStyle} src="/icons/logo-dark.svg"/></div>}
-
-        <div className={currentBoardLabel}>{selectedBoard}</div>
-        
-        <Button variant={ButtonVariant.secondary}>+ Add New Task</Button>
-        <Button variant={ButtonVariant.empty}><img src="/icons/icon-vertical-ellipsis.svg" /></Button>
+  const { isDarkMode } = props;
+  const { selectedBoard } = useContext(BoardContext);
+  return (
+    <div className={headerStyles}>
+      {isDarkMode ? (
+        <div className={logoWrapper}>
+          <img className={logoStyle} src="/icons/logo-light.svg" />{' '}
         </div>
-    )
+      ) : (
+        <div className={logoWrapper}>
+          <img className={logoStyle} src="/icons/logo-dark.svg" />
+        </div>
+      )}
+
+      <div className={currentBoardLabel}>{selectedBoard}</div>
+
+      <Button variant={ButtonVariant.secondary}>+ Add New Task</Button>
+      <Button variant={ButtonVariant.empty}>
+        <img src="/icons/icon-vertical-ellipsis.svg" />
+      </Button>
+    </div>
+  );
 }
